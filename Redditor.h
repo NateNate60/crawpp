f9fb3147dfc1d3dc4b3be8823cd7e6edb44c31eb
NateNate60 <nathanlim2004@gmail.com>
2022-07-12 18:30:56 -0700

    Cleanup; cache info to avoid multiple API calls

--- Redditor.h
+++ Redditor.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <ctime>
+#include <nlohmann/json.hpp>
 
 #include "Reddit.h"
 
@@ -10,7 +11,8 @@ namespace CRAW {
     */
     class Redditor {
         private:
-            Reddit * _redditinstance;
+            const Reddit * _redditinstance;
+            nlohmann::json _information;
         public:
             // The user's username, without the u/
             std::string username;
@@ -25,36 +27,23 @@ namespace CRAW {
             // The number of karma points earned from getting awards
             unsigned long awardeekarma;
             // The number of karma points earned from posts
-            unsigned long linkkarma;
+            unsigned long postkarma;
             // The number of karma points earned from comments
             unsigned long commentkarma;
 
             /**
             Initialise a Redditor instance matching the given username.
-            In most cases, where the ability to interact with the Redditor is needed, use Reddit.redditor() instead.
             
             @param name: The username of the Redditor
             */
-            Redditor(const std::string & name);
-
-            /**
-            Initialise a Redditor instance matching the given username.
-            In most cases, where the ability to interact with the Redditor is needed, use Reddit.redditor() instead.
-            
-            @param name: The username of the Redditor
-            */
-            Redditor(const Reddit * redditinstance, const std::string & name);
+            Redditor (const std::string & name, const Reddit * redditinstance = nullptr);
 
             /**
             The [] operator is used to fetch information about a user. All information is returned as a std::string.
             Some commonly-used attributes can be fetched using the dot operator (.), but all information can be fetched using this.
+            If the value is an array or an object, it will be given as a JSON string.
             */
             std::string operator[] (const std::string & attribute);
-
-            /*
-            Returns whether two Redditor are the same (i.e. have the same name)
-            */
-            bool operator== (const Redditor & redditor);
 
             /** 
              * Follow this Redditor
