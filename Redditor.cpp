63995bb772563455aa3956ced93a21bf135ac546
NateNate60 <nathanlim2004@gmail.com>
2022-07-14 14:22:42 -0700

    Implement Redditor class member functions

---
+++ Redditor.cpp
@@ -0,0 +1,81 @@
+#include <restclient-cpp/restclient.h>
+#include <restclient-cpp/connection.h>
+#include <nlohmann/json.hpp>
+
+#include "Redditor.h"
+#include "crawexceptions.hpp"
+
+using namespace CRAW;
+
+Redditor::Redditor (const std::string & name, Reddit * redditinstance) {
+    
+    nlohmann::json responsejson;
+    std::string url = "https://api.reddit.com/user/";
+    url += name;
+    url += "/about.api";
+
+    RestClient::Response response = RestClient::get(url);
+    switch (response.code){
+        case 404:
+            throw NotFoundError("Could not find any user with that username.");
+        case 403:
+            throw UnauthorisedError("You aren't allowed to do that. Did you block this user?");
+        default:
+            responsejson = nlohmann::json::parse(response.body)["data"];
+    }
+    
+    _redditinstance = redditinstance;
+    username = responsejson["name"];
+    created = responsejson["created"];
+    totalkarma = responsejson["total_karma"];
+    awardeekarma = responsejson["awardee_karma"];
+    awarderkarma = responsejson["awarder_karma"];
+    commentkarma = responsejson["comment_karma"];
+    postkarma = responsejson["link_karma"];
+    _information = responsejson;
+}
+
+std::string Redditor::operator[] (const std::string & attribute) {
+    nlohmann::json value = _information[attribute];
+    if (value.is_null()) {
+        throw std::invalid_argument("Attribute " + attribute + " doesn't exist.");
+    }
+    if (value.is_boolean()) {
+        return value ? "true" : "false";
+    }
+    if (value.is_number()) {
+        return std::to_string((double) value);
+    }
+    if (value.is_string()) {
+        return value;
+    }
+    // This means the value is an object or an array (which is technically also an object)
+    // return it as a JSON-string
+    return value.dump();
+}
+
+void Redditor::follow () {
+    if (_redditinstance == nullptr) {
+        throw NotLoggedInError("You must be logged in to follow someone.");
+    }
+    nlohmann::json body;
+    body["action"] = "follow";
+    body["action_source"] = "g"; // This is arbitrary; IDK why the API requires it
+    body["skip_initial_defaults"] = true;
+    body["sr_name"] = "u_" + username;
+
+    _redditinstance->_sendrequest("POST", "/api/subscribe", body.dump());
+}
+
+void Redditor::unfollow () {
+    if (_redditinstance == nullptr) {
+        throw NotLoggedInError("You must be logged in to follow someone.");
+    }
+    nlohmann::json body;
+    body["action"] = "unfollow";
+    body["action_source"] = "g"; // This is arbitrary; IDK why the API requires it
+    body["skip_initial_defaults"] = true;
+    body["sr_name"] = "u_" + username;
+
+    _redditinstance->_sendrequest("POST", "/api/subscribe", body.dump());
+}
--- crawexceptions.hpp
+++ crawexceptions.hpp
@@ -2,7 +2,7 @@
 #include <stdexcept>
 
 namespace CRAW {
-    /*
+    /**
     Certain features are only available if authentication information is provided, 
     e.g. making a post. If this information is not provided and the user attempts 
     to perform some action which requires authentication, NotLoggedInError will be
@@ -14,7 +14,7 @@ namespace CRAW {
             explicit NotLoggedInError (const char * what_arg);
     };
 
-    /*
+    /**
     An error occurred while trying to retrive the token
     */
     class LoginError : public std::runtime_error {
@@ -23,7 +23,7 @@ namespace CRAW {
             explicit LoginError (const char * what_arg);
     };
 
-    /*
+    /**
     Some tasks can only be perform with authorisation. Anything that causes the API
     to return a 403 will throw UnauthorisedError. Examples of this is trying to
     delete a post when you aren't the author or trying to remove a comment when you
@@ -35,7 +35,7 @@ namespace CRAW {
             explicit UnauthorisedError (const char * what_arg);
     };
 
-    /*
+    /**
     Trying to access something that doesn't exist (or giving a monkey a shower,
     surfing tidal waves, creating nanobots, or looking in Frankenstien's brain)
     will cause NotFoundError to be thrown.
@@ -44,5 +44,16 @@ namespace CRAW {
         public:
             explicit NotFoundError (const std::string & what_arg);
             explicit NotFoundError (const char * what_arg);
+    };
+
+    /**
+     * Whenever the user tries to interact with themselves in a way that is
+     * invalid, such as trying to ban themselves or follow themselves, throw
+     * InvalidInteractionError.
+     */
+    class InvalidInteractionError : public std::runtime_error {
+        public:
+            explicit InvalidInteractionError (const std::string & what_arg);
+            explicit InvalidInteractionError (const char * what_arg);
     };
 }
