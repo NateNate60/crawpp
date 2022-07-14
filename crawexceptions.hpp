a30da6a2c7f88a78dca167bdd8e31db93b737106
NateNate60 <nathanlim2004@gmail.com>
2022-07-14 14:17:17 -0700

    Add LoginError exception

--- crawexceptions.hpp
+++ crawexceptions.hpp
@@ -15,6 +15,15 @@ namespace CRAW {
     };
 
     /*
+    An error occurred while trying to retrive the token
+    */
+    class LoginError : public std::runtime_error {
+        public:
+            explicit LoginError (const std::string & what_arg);
+            explicit LoginError (const char * what_arg);
+    };
+
+    /*
     Some tasks can only be perform with authorisation. Anything that causes the API
     to return a 403 will throw UnauthorisedError. Examples of this is trying to
     delete a post when you aren't the author or trying to remove a comment when you
