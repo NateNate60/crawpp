ea13e1660f4293f76c0873af49d8c2331bfb4fb5
NateNate60 <nathanlim2004@gmail.com>
2022-07-26 18:42:21 -0700

    Add craw.h for easy inclusion in tests

---
+++ craw.h
@@ -0,0 +1,12 @@
+/*
+The sole purpose of this file is to #include all the other
+header files to make it easier to test this library.
+*/
+
+#include "Reddit.h"
+#include "Redditor.h"
+#include "Subreddit.h"
+#include "Submission.h"
+#include "Post.h"
+#include "Comment.h"
+#include "crawexceptions.hpp"
