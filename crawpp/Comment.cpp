710ac5bae489f7c419a10025ac57228a1464765e
NateNate60 <nathanlim2004@gmail.com>
2022-08-09 10:57:16 -0700

    Use proper JSON extraction methods

--- crawpp/Comment.cpp
+++ crawpp/Comment.cpp
@@ -8,20 +8,20 @@
 namespace CRAW {
     Comment::Comment (const nlohmann::json & data, Reddit * redditinstance) {
         information = data;
-        id = data["id"];
-        authorname = data["author"];
-        fullname = data["name"];
-        posted = static_cast<time_t>(data["created"]);
-        score = data["score"];
-        subredditname = data["subreddit"];
+        id = data["id"].get<std::string>();
+        authorname = data["author"].get<std::string>();
+        fullname = data["name"].get<std::string>();
+        posted = data["created"].get<int>();
+        score = data["score"].get<int>();
+        subredditname = data["subreddit"].get<std::string>();
         type = "comment";
         if (data["edited"].is_number()) {
-            edited = data["edited"];
+            edited = data["edited"].get<time_t>();
         } else {
             edited = 0;
         }
-        depth = data["depth"];
-        content = data["body"];
+        depth = data["depth"].get<int>();
+        content = data["body"].get<std::string>();
 
         awards = std::set<Award> ();
         for (auto & i : data["all_awardings"]) {
