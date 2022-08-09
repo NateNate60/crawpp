709c908e282e2cdf948c8feb84d429a17a72a508
NateNate60 <nathanlim2004@gmail.com>
2022-08-08 18:17:24 -0700

    Add CRAWObject header

---
+++ include/crawpp/CRAWObject.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace CRAW {
+    class Reddit;
+
+    /**
+     * @brief A base class that all CRAW models inherit from.
+     * @warning This class should not be instantiated on its own.
+     * 
+     */
+    class CRAWObject {
+        protected:
+            Reddit * _redditinstance;
+    };
+}
